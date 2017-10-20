8876a994d98858a6d2c79f0710d553e3067a62d3
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-10-20 14:59:32 -0500

    Adds filter-map-reduce example

--- map-reduce/src/main.rs
+++ map-reduce/src/main.rs
@@ -16,6 +16,19 @@ fn run() -> Result<()> {
     let word_count: i32 = sentences.par_iter().map(|_x| 1).reduce(|| 0, |x, y| x + y);
     println!("Word count: {}", word_count);
 
+    // filter - map - reduce example
+    let v: Vec<i32> = vec![1, 2, 7, 3, 9, 3, 8, 4, 9, 11, 1, 2, 12];
+    let expected = 1 + 4 + 9 + 9 + 16 + 1 + 4;
+    let square_sum_lt_5 = v.par_iter().filter(|&&x| x < 5).map(|&x| x * x).reduce(
+        || 0,
+        |x, y| x + y,
+    );
+    println!(
+        "Sum of squared values for elements less than 5 is {} (expected value: {})",
+        square_sum_lt_5,
+        expected
+    );
+
     Ok(())
 }
 
